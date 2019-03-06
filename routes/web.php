59413e63f89236d645bcc562153cffe974ad95a9
root <root@homestead>
2019-03-06 11:33:12 +0000

    init auth system

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
     Route::get('/', 'IndexController@index')->name('main');
+    Route::get('/home', 'HomeController@index')->name('home');
     Auth::routes();
     Route::get('/news', 'NewsController@index')->name('news');
     Route::get('/all-news', 'NewsController@allnews')->name('news.all');
     Route::get('/{rubric}/{page}', 'RubricController@listing')->where(['rubric' => '[a-z0-9\-]+', 'page' => '[0-9]+'])->name('rubric.listing');
     Route::get('/{rubric}', 'RubricController@index')->where('rubric', '[a-z0-9\-]+')->name('rubric');
     Route::get('/{rubric}/{publication}', 'PublicationController@show')->where(['rubric' => '[a-z0-9\-]+', 'publication' => '[a-z0-9\-\.]+'])->name('publication.show');
-    Route::get('/home', 'HomeController@index')->name('home');
+
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
